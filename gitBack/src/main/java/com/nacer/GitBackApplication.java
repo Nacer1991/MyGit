38eaa42365ee9053e47bc8a9930ef250234c0bc8
Your Name <you@example.com>
2019-02-03 23:09:35 +0100

    premier commit

---
+++ gitBack/src/main/java/com/nacer/GitBackApplication.java
@@ -0,0 +1,14 @@
+package com.nacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBackApplication.class, args);
+	}
+
+}
+
